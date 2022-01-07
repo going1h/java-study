478fce1b085fc71f34d607bdffd224977dc13841
going1h <rnjs8608@gmail.com>
2022-01-07 18:13:59 +0900

    java-study : basic question

--- src/com/kdh/javastudy03/javastudy03.java
+++ src/com/kdh/javastudy03/javastudy03.java
@@ -1,5 +1,6 @@
 package com.kdh.javastudy03;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class javastudy03 {
@@ -147,16 +148,85 @@ public class javastudy03 {
 	//	년도 입력 : 2004
 	//	윤년
 	
-	System.out.print("년도 입력 : ");
-	int year = sc.nextInt();
+		System.out.print("년도 입력 : ");
+		int year = sc.nextInt();
+		
+		if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+			System.out.println(year + "년은 윤년 입니다.");
+		} else {
+			System.out.println(year + "년은 평년 입니다.");
+		}
+		System.out.print("------------------");
+		System.out.println();
 	
-	if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
-		System.out.println(year + "년은 윤년 입니다.");
-	} else {
-		System.out.println(year + "년은 평년 입니다.");
-	}
+	
+	//	[문제14] 50개의 난수를 만들어서 다음과 같이 출력하시오
+	//    조건1) 0 - 100 사이의 수 일것
+	//    조건2) 1줄에 6개씩 표시하시오  
+	//
+	//	--출력--
+	//	12      72      74      0       20      89
+	//	84      83      53      0       95      53
+	//	68      55      89      6       20      68
+	//	17      9       75      56      17      85
+	//	62      33      7       34      85      28
+	//	28      55      95      31      69      22
+	//	30      92      1       16      88      80
+	//	64      55      71      12      29      72
+	//	20      41
+	//	합=2420
+	
+		int num14;
+		int sum14 = 0;
+	
+		for(int i=1; i<=50; i++) {
+			num14 = new Random().nextInt(101);
+			System.out.print(num14 + "\t");
+			sum14 += num14;
+			if(i % 6 == 0)
+				System.out.println();
+		}
+		System.out.println("\n합 = " + sum14);
+		
+		System.out.print("------------------");
+		System.out.println();
+		
+//		[문제15] 2자리의 난수를 발생하여 발생된 숫자를 맞추는 게임 프로그램
+//      난수발생)1. int a=(int)(Math.random()*100) : 0 - 99 사이의 난수
+//             2. ① import java.util.Random;
+//             ② Random 변수1 = new Random();
+//             ③ int 변수2 = 변수1.nextInt(최대값); 
+//             ==>  0 ~ (최대값 - 1)사이의 임의의 수
+//
+//		[실행결과]
+//		
+//		*** 난수가 발생하였습니다...맞추어 보세요...*** 
+//		
+//		숫자 입력 : 50
+//		컴퓨터의 숫자가 더 큽니다..
+//		
+//		숫자 입력 : 90
+//		컴퓨터의 숫자가 더 작습니다..
+//		
+//		         :
+//		
+//		숫자 입력 : 83
+//		추카추카...xx번만에 맞추셨습니다...
+		
+		int a = (int)(Math.random()*100);
+		int num, cnt = 0;
+		
+		while(true) {
+			System.out.print("숫자입력 : ");	num = sc.nextInt();
+			cnt++;
+			if(num>a)
+				System.out.println("컴퓨터의 숫자가 더 작습니다.\n");
+			else if(num<a)
+				System.out.println("컴퓨터의 숫자가 더 큽니다.\n");
+			else
+				break;
+		}
+		System.out.println("추카추카~! " + cnt + "번만에 맞추셨습니다.");
 		
 	}
-	
-	
 }
